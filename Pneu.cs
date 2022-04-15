44479ebbe85a2de5819f4034b670132103f86757
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-15 08:16:21 -0300

    Pneu

---
+++ Pneu.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Pneu {
+    static void Main () {
+        var A = int.Parse(Console.ReadLine());
+        var B = int.Parse(Console.ReadLine());
+        
+       //Termine o código 
+       
+       Console.Write(A - B);
+        
+    }
+}
