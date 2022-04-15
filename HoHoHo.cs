8f530fdc66a90f3566fe975c1a32d624f1255544
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-15 08:11:10 -0300

    HoHoHo

---
+++ HoHoHo.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace DIO {
+    class Program {
+        static void Main(string[] args) {
+           
+            int n = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < n - 1; i++) 
+                {
+                    Console.Write("Ho "); //Complete o código no espaço em branco
+                }
+                
+                if (n !=0)
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
